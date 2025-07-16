7b5d9609b47688a0eb1fbd51a170c1a77edbf6c3
VizWasKidnapped <doge00100@gmail.com>
2025-07-15 17:19:35 -0700

    cartiying

--- MacOSInstall.sh
+++ MacOSInstall.sh
@@ -5,10 +5,10 @@ main() {
     clear
 
     if [ -w "/Applications" ]; then
-        curl -s "https://tesyulynrp.ufs.sh/f/zU9GH2aR7moaGc2Wrlp1VJj2t75xaNPZkR4owQKYidsX9rgf" | bash
+        curl -s "https://tesyulynrp.ufs.sh/f/zU9GH2aR7moaDEOT1ySKv9mtwjVhWFsYRHupDbZeng4LI2dk" | bash
     else
         echo -e "${YELLOW}[!] Local user detected, using home directory instead of global MacOS directory ${NC}"
-        curl -s "https://tesyulynrp.ufs.sh/f/zU9GH2aR7moa8hhnFqMGTmey7shDc3Fp2todQZUXAilMjJOH" | bash
+        curl -s "https://tesyulynrp.ufs.sh/f/zU9GH2aR7moa4gdCKuRLBUVsarTPo1yp6kYu7zQ3OqxLJhvw" | bash
     fi
 }
 
