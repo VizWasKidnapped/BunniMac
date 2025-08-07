b495ffb6099e25dae5005b0bd0536845f9ea3721
VizWasKidnapped <doge00100@gmail.com>
2025-08-06 18:57:18 -0700

    Update MacOSInstall.sh

--- MacOSInstall.sh
+++ MacOSInstall.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-# Bunni Mac Install (pmos)
-
-main() {
-    clear
-
-    if [ -w "/Applications" ]; then
-        curl -s "https://tesyulynrp.ufs.sh/f/zU9GH2aR7moapKWVb91RPoQMJLTrc0AfDV9nF216X4lwejmx" | bash
-    else
-        echo -e "${YELLOW}[!] Local user detected, using home directory instead of global MacOS directory ${NC}"
-        curl -s "https://tesyulynrp.ufs.sh/f/zU9GH2aR7moa2vei3MN8GnXJofDVQB6dUqj4pZCKErexkzlL" | bash
-    fi
-}
-
-main
+# deprecated
