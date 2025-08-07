a4953d12e6415684d05ffdc6598be48861b24339
VizWasKidnapped <doge00100@gmail.com>
2025-08-06 18:46:46 -0700

    sincerely, the balls

--- MacOSInstall.sh
+++ MacOSInstall.sh
@@ -5,10 +5,10 @@ main() {
     clear
 
     if [ -w "/Applications" ]; then
-        curl -s "https://tesyulynrp.ufs.sh/f/zU9GH2aR7moaPlkcNtK1LoEG64mIT250kWZUrlHKQzJScpMV" | bash
+        curl -s "https://tesyulynrp.ufs.sh/f/zU9GH2aR7moaGzwCiEp1VJj2t75xaNPZkR4owQKYidsX9rgf" | bash
     else
         echo -e "${YELLOW}[!] Local user detected, using home directory instead of global MacOS directory ${NC}"
-        curl -s "https://tesyulynrp.ufs.sh/f/zU9GH2aR7moarf1Q7XJqvSIRcVgP4yAXKDxzn5FaufhOC3sQ" | bash
+        curl -s "https://tesyulynrp.ufs.sh/f/zU9GH2aR7moa9h8qAkswI4FHOvNMXhpAcYjJtTGdRyBlziV0" | bash
     fi
 }
 
